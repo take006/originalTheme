d3a8a61e89f6d184629c4ae9db8af14d4bc08ed0
take088917761@gmail.com <yuyalikeswimming@icloud.com>
2025-06-14 14:39:14 +0900

    Fix: footer layout adjustment

--- footer.php
+++ footer.php
@@ -1,5 +1,11 @@
     <footer class="footer">
-      <a href="<?php echo home_url('/privacy-policy'); ?>">privacy policy</a>
+      <div class="footer-menu">
+        <p class="footer-title">サポート</p>
+        <div class="support-wrapper">
+          <a href="#">利用規約</a>
+          <a href="<?php echo home_url('/privacy-policy'); ?>">プライバシーポリシー</a>
+        </div>
+      </div>
       <div class="copy-right-wrapper">
         <p><strong>ITnote</strong></p>
         <p class ="copy-right">&copy; Copyright 2025 ITnote. All rights reserved</p>
